38e9f46f1af95fd3cf2e2518eb216c86dda35bf8
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2022-01-20 12:04:08 -0800

    comment

--- mysql/Database.php
+++ mysql/Database.php
@@ -151,7 +151,7 @@ function select($query) {
     $isLimit1 = $parts["limit"] == 1;
 	
     // Needs to be title case.
-	$table = ucwords($parts["from"]);
+	$table = ucwords($parts["from"]);  // this doesn't work if the string in not lowercase????
 
     $result = Database::query($query);
 
